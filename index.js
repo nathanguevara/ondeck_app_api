fc7d96d1a8ea899ff0c8bba38b046e4d544ff430
nathanguevara <nathan.guevara@gmail.com>
2022-09-30 12:21:49 -0500

    cors 5

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 app.use("/api", jokesRouter);
 
 
