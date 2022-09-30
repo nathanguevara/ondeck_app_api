ccd388c4ab731ae37b690bc00ef6c51205ac25d8
nathanguevara <nathan.guevara@gmail.com>
2022-09-30 11:12:35 -0500

    cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
 const jokesRouter = require('./routes/jokes')
 
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use("/api", jokesRouter);
+app.use(cors());
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
